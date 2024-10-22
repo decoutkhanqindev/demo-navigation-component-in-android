f9d7def4d793906488a67f1ceaa661aa942ed61c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-23 02:04:46 +0700

    handle type-safe access to arguments specified for destinations and actions.

--- app/src/main/java/com/example/demonavigationcomponetinandroid/MyHomeFragment.kt
+++ app/src/main/java/com/example/demonavigationcomponetinandroid/MyHomeFragment.kt
@@ -5,17 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
 import com.example.demonavigationcomponetinandroid.databinding.FragmentMyHomeBinding
 
-class MyHomeFragment: Fragment() {
+class MyHomeFragment : Fragment() {
   private var _binding: FragmentMyHomeBinding? = null
   private val binding: FragmentMyHomeBinding get() = _binding!!
   
   override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
+    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
   ): View {
     _binding = FragmentMyHomeBinding.inflate(inflater, container, false)
     return binding.root
@@ -24,8 +23,25 @@ class MyHomeFragment: Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     
-    binding.button.setOnClickListener {
-      findNavController().navigate(R.id.action_myHomeFragment_to_mySecondFragment)
+    binding.apply setOnClickListener@{
+      button.setOnClickListener {
+        val number: String = number.text.toString()
+        val message: String = message.text.toString()
+        val user = User(
+          userId.text.toString(), userName.text.toString(), userAddress.text.toString()
+        )
+        
+        // safe-args way
+        val direction: NavDirections =
+          MyHomeFragmentDirections.actionMyHomeFragmentToMySecondFragment(user, number, message)
+        findNavController().navigate(direction)
+        
+//        // bundle way
+//        findNavController().navigate(
+//          resId = R.id.action_myHomeFragment_to_mySecondFragment,
+//          args = bundleOf("number" to number, "message" to message, "user" to user)
+//        )
+      }
     }
   }
   
--- app/src/main/java/com/example/demonavigationcomponetinandroid/MySecondFragment.kt
+++ app/src/main/java/com/example/demonavigationcomponetinandroid/MySecondFragment.kt
@@ -1,31 +1,62 @@
 package com.example.demonavigationcomponetinandroid
 
+import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.demonavigationcomponetinandroid.databinding.FragmentMySecondBinding
+import kotlin.math.log
 
-class MySecondFragment: Fragment() {
+class MySecondFragment : Fragment() {
   private var _binding: FragmentMySecondBinding? = null
   private val binding: FragmentMySecondBinding get() = _binding!!
   
+  private val args: MySecondFragmentArgs by navArgs()
+  
+  private var user: User? = null
+  
+  @RequiresApi(Build.VERSION_CODES.TIRAMISU)
   override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
+    inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
   ): View {
     _binding = FragmentMySecondBinding.inflate(inflater, container, false)
     return binding.root
   }
   
+  @SuppressLint("SetTextI18n")
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     
-    binding.button.setOnClickListener {
-      findNavController().navigate(R.id.action_mySecondFragment_to_myThirdFragment)
+    binding.apply {
+      // safe-args way
+      number.text = args.number
+      message.text = args.message
+
+      user = args.user
+      userId.text = user?.id
+      userName.text = user?.name
+      userAddress.text = user?.address
+      
+//      // bundle way
+//      number.text = arguments?.getString("number")
+//      message.text = arguments?.getString("message")
+//
+//      user = arguments?.getParcelable("user")
+//      userId.text = user?.id
+//      userName.text = user?.name
+//      userAddress.text = user?.address
+      
+      button.setOnClickListener {
+        findNavController().navigate(R.id.action_mySecondFragment_to_myThirdFragment)
+      }
     }
   }
   
