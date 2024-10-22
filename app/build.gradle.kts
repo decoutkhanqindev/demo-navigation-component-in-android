5ea8555b136ae6aa90521ae06c125cb0a0d98b8c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-10-23 02:04:13 +0700

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,11 +25,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "17"
   }
   
   buildFeatures {
@@ -42,7 +42,6 @@ dependencies {
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.appcompat)
   implementation(libs.material)
-  implementation(libs.androidx.fragment.ktx)
   implementation(libs.androidx.activity)
   implementation(libs.androidx.constraintlayout)
   testImplementation(libs.junit)
@@ -54,4 +53,5 @@ dependencies {
   implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
   implementation("androidx.navigation:navigation-fragment:$nav_version")
   implementation("androidx.navigation:navigation-ui:$nav_version")
+  implementation(libs.androidx.fragment.ktx)
 }
